b7a8f469fa2169f8da0db95ab0e381b32d43a7c2
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-04-08 20:00:48 +0000

    start trying to compile the thing

--- qof/src/qofsql.c
+++ qof/src/qofsql.c
@@ -26,43 +26,11 @@
     @author Copyright (C) 2004 Linas Vepstas <linas@linas.org>
 */
 
-#ifndef QOF_SQL_QUERY_H
-#define QOF_SQL_QUERY_H
-
 #include <glib.h>
+#include <libsql/sql_parser.h>
 #include "qofbook.h"
-
-typedef struct _QofSqlQuery QofSqlQuery;
-
-/** Create a new SQL-syntax query machine.
- */
-QofSqlQuery * qof_sql_query_new (void);
-void qof_sql_query_destroy (QofSqlQuery *);
-
-/** Set the book to be searched (you can search multiple books)
- *  If no books are set, no results will be returned (since there
- *  is nothing to search over).
- */
-void qof_sql_query_set_book (QofSqlQuery *q, QofBook *book);
-
-/** Perform the query, return the results.
- *
- *  The returned list is a list of the 'search-for' type that was
- *  previously set with the qof_query_search_for() or the
- *  XXX fixme this doc is wrong.
- *  qof_query_create_for() routines.  The returned list will have
- *  been sorted using the indicated sort order, and trimed to the
- *  max_results length.
- *  Do NOT free the resulting list.  This list is managed internally
- *  by QofSqlQuery.
- */
-
-GList * qof_sql_query_run (QofSqlQuery *query, const char * str);
-
-#endif /* QOF_SQL_QUERY_H */
-
-#include <libqofsql/sql_parser.h>
 #include "qofquery.h"
+#include "qofsql.h"
 
 struct _QofSqlQuery
 {
@@ -73,7 +41,7 @@ struct _QofSqlQuery
 QofSqlQuery *
 qof_sql_query_new(void)
 {
-	QofSqlQuery * sqn = (QofSqlQuery *) g_new (QofSqlQuery);
+	QofSqlQuery * sqn = (QofSqlQuery *) g_new (QofSqlQuery, 1);
 	
 	sqn->qof_query = qof_query_create ();
 	sqn->parse_result = NULL;
@@ -86,7 +54,7 @@ qof_sql_query_destroy (QofSqlQuery *q)
 {
 	if (!q) return;
 	qof_query_destroy (q->qof_query);
-	sql_destroy (sqn->parse_result);
+	sql_destroy (q->parse_result);
 	g_free (q);
 }
 
@@ -95,6 +63,21 @@ qof_sql_query_set_book (QofSqlQuery *q, QofBook *book)
 {
 	if (!q) return;
 	qof_query_set_book (q->qof_query, book);
+}
+
+static inline void
+get_table_and_param (char * str, char **tab, char **param)
+{
+	char * end = strchr (str, '.');
+	if (!end) 
+	{
+		*tab = 0;
+		*param = str;
+		return;
+	}
+	*end = 0;
+	*tab = str;
+	*param = end+1;
 }
 
 GList * 
@@ -118,36 +101,38 @@ qof_sql_query_run (QofSqlQuery *query, const char *str)
 	}
 
 	sql_select_statement *sss = query->parse_result->statement;
-	sql_where swear = sss->where;
+	sql_where * swear = sss->where;
 	if (NULL == swear)
 	{
 		printf ("expecting 'where' statement\n");
 		return NULL;
 	}
 
-	switch (swear)
+	switch (swear->type)
 	{
 		case SQL_pair:
-			printf ("duuude unhandled\n");
+			printf ("duuude unhandled sql pair\n");
 			break;
 		case SQL_negated:
-			printf ("duuude unhandled\n");
+			printf ("duuude unhandled sql negated\n");
 			break;
 		case SQL_single:
 		{
 			GSList *param_list;
-			QofQueryPredData *pred_data;
+			QofQueryPredData *pred_data = NULL;
 			
-			/* field to match, assumed, for now to be on the left */
-			/* XXX fix the left-right thing */
-			param_list = qof_query_build_param_list (where->d.pair.left, NULL);
-
-			sql_condition * cond = where->d.single;
+			sql_condition * cond = swear->d.single;
 			if (NULL == cond)
 			{
 				printf ("missing condition\n");
 				return NULL;
 			}
+			
+			/* field to match, assumed, for now to be on the left */
+			/* XXX fix the left-right thing */
+			param_list = qof_query_build_param_list (cond->d.pair.left, NULL);
+
+			/* get the where-term comparison operator */
 			QofQueryCompare qop;
 			switch (cond->op)
 			{
@@ -159,42 +144,96 @@ qof_sql_query_run (QofSqlQuery *query, const char *str)
 				case SQL_diff:  qop = QOF_COMPARE_NEQ; break;
 			}
 			
-			pred_data = qof_query_string_predicate (qop, /* comparison to make */
+			/* OK, need to know the type of the thing being matched 
+			 * in order to build the correct predicate.  Get the type 
+			 * from the object parameters. */
+			char *table_name;
+			char *param_name;
+			get_table_and_param (cond->d.pair.left, &table_name, &param_name);
+			if (NULL == table_name)
+			{
+				printf ("error: unsupported, naed a table name for now \n");
+				// XXX if no table here, we should use the global query table.
+				// unless there's more than one global query table, and 
+				// then its indeterminate again.
+				return NULL;
+			}
+			
+			QofType param_type = qof_class_get_parameter_type (table_name,
+			                  param_name);
 
-		 "M M M My Sharona",                /* string to match */
-	          QOF_STRING_MATCH_CASEINSENSITIVE,  /* case matching */
-	         FALSE);                            /* use_regexp */
-
-			printf ("duuude unhandled\n");
-			break;
+			switch (param_type)
+			{
+				case QOF_TYPE_STRING:
+					pred_data = 
+					    qof_query_string_predicate (qop, /* comparison to make */
+					    cond->d.pair.right,   /* string to match */
+	          	    QOF_STRING_MATCH_CASEINSENSITIVE,  /* case matching */
+	         	    FALSE);                            /* use_regexp */
+					break;
+				case QOF_TYPE_DATE:
+				case QOF_TYPE_NUMERIC:
+				case QOF_TYPE_DEBCRED:
+				case QOF_TYPE_GUID:
+				case QOF_TYPE_INT32:
+				case QOF_TYPE_INT64:
+				case QOF_TYPE_DOUBLE:
+				case QOF_TYPE_BOOLEAN:
+				case QOF_TYPE_KVP:
+				case QOF_TYPE_CHAR:
+					printf ("Error: predicate type unsupported for now \n");
+					return NULL;
+					break;
+			}
+			qof_query_add_term (query->qof_query, 
+			                param_list, pred_data, QOF_QUERY_FIRST_TERM);
 		}
 	}
 
+	/* We also want to set the type of thing to search for.
+	 * If the user said SELECT * FROM ... then we should return
+	 * a list of QofEntity.  Otherwise, we return ... ?
+	 * XXX all this needs fixing.
+	 */
 	GList *tables = sql_statement_get_tables (query->parse_result);
-	GList *fields = sql_statement_get_fields (query->parse_result);
+	for (node=tables; node; node=node->next)
+	{
+		char * tablename = node->data;
+		qof_query_search_for (query->qof_query, tablename);
+		break;
+	}
 
+	GList *results = qof_query_run (query->qof_query);
+
+	return results;
+
+#if 0
+	GList *fields = sql_statement_get_fields (query->parse_result);
 	for (node=fields; node; node=node->next)
 	{
 		char *fieldname = node->data;
 	}
 
+
 query: select * from myobj where myobj.thing=1;
 		   fields:
 		     *
-						 *   from:
-						 *       table: myobj
-						 *         where:
-						 *               op: =
-						 *                     left:
-						 *                             myobj.thing
-						 *                                   right:
-						 *                                           1
-						 *                                            
-						 *                                            Tables: myobj
-						 *                                            Fields: *
-						 *
+			from:
+			   table: myobj
+			   where:
+			     op: =
+			    left:
+			myobj.thing
+			   right:
+		        1
+		                      
+		       Tables: myobj
+		      Fields: *
+		 
+#endif
 }
 
+#if 0
 int main ()
 {
 	char * str = "SELECT * from MyObject";
@@ -203,8 +242,6 @@ int main ()
 			  
 	qof_sql_query_set_book (qsq,...);
 
-	qof_sql_query_run (qsq, str);
-
-
+	GList * results = qof_sql_query_run (qsq, str);
 }
-
+#endif
