320c4f513edd8f55960b69e04375710ff0edb677
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-04-19 00:54:27 +0000

    start cleaing up

--- qof/examples/gobject/qofgobj.c
+++ qof/examples/gobject/qofgobj.c
@@ -33,6 +33,9 @@
 /** The API defined in this file allows a user to register any
  *  GLib GObject (and any object derived from one, e.g. GTK/Gnome)
  *  with the QOF system so that it becomes searchable.  
+ *
+ * XXX Only GObject properties are searchable, data and other 
+ * hanging off the GObject is not.  Fix this.
  */  
 
 #include <glib-object.h>
@@ -44,8 +47,8 @@ void qof_gobject_init(void);
 void qof_gobject_shutdown (void);
 
 /** Register a GObject class with the QOF subsystem.
- *  Doing this will make this GObject searchable using the
- *  QOF subsystem.
+ *  Doing this will make the properties associated with
+ *  this GObject searchable using the QOF subsystem.
  *
  *  The QofType can be any string you desire, although typically
  *  you might want to set it to G_OBJECT_CLASS_NAME() of the 
@@ -75,9 +78,13 @@ void qof_gobject_register (QofType type, GObjectClass *obclass);
 void qof_gobject_register_instance (QofBook *book, QofType, GObject *);
 
 #endif /* QOF_GOBJ_H */
+/** @} */
 
+#include <qof/gnc-trace.h>
 #include <qof/qof.h>
 #include <stdio.h>
+
+static short module = MOD_QUERY;
 
 static gboolean initialized = FALSE;
 static GSList *paramList = NULL;
@@ -152,8 +159,19 @@ qof_gobject_getter (gpointer data, QofParam *getter)
 {
 	GObject *gob = data;
 
-printf ("duude trying to get type %s\n", getter->param_type);
-	// damn I need lambda .... 
+	GParamSpec *gps = getter->param_userdata;
+
+	if (G_IS_PARAM_SPEC_STRING(gps))
+	{
+		GValue gval = {G_TYPE_INVALID};
+		g_value_init (&gval, G_TYPE_STRING);
+		g_object_get_property (gob, getter->param_name, &gval);
+
+      const char * str = g_value_get_string (&gval);
+      return (gpointer) str;
+   }
+
+	PWARN ("unhandled parameter type %s", G_PARAM_SPEC_TYPE_NAME(gps));
 	return NULL;
 }
 
@@ -186,26 +204,74 @@ qof_gobject_register (QofType e_type, GObjectClass *obclass)
 	QofParam * qof_param_list = g_new0 (QofParam, n_props);
 	paramList = g_slist_prepend (paramList, qof_param_list);
 
-printf ("got %d props\n", n_props);
+	PINFO ("object %s has %d props", e_type, n_props);
 	int i, j=0;
 	for (i=0; i<n_props; i++)
 	{
 		GParamSpec *gparam = prop_list[i];
 		QofParam *qpar = &qof_param_list[j];
 
-		printf ("%d %s\n", i, gparam->name);
+		PINFO ("param %d %s is type %s", 
+		      i, gparam->name, G_PARAM_SPEC_TYPE_NAME(gparam));
 
 		qpar->param_name = g_param_spec_get_name (gparam);
 		qpar->param_getfcn = qof_gobject_getter;
 		qpar->param_setfcn = NULL;
-		if (G_IS_PARAM_SPEC_INT(gparam))
+		qpar->param_userdata = gparam;
+		if ((G_IS_PARAM_SPEC_INT(gparam))  ||
+		    (G_IS_PARAM_SPEC_UINT(gparam)) ||
+		    (G_IS_PARAM_SPEC_ENUM(gparam)) ||
+		    (G_IS_PARAM_SPEC_FLAGS(gparam)))
 		{
 			qpar->param_type = QOF_TYPE_INT32;
-printf ("its an int!! %s \n", qpar->param_name);
 			j++;
+		} 
+		else
+		if ((G_IS_PARAM_SPEC_INT64(gparam)) ||
+		    (G_IS_PARAM_SPEC_UINT64(gparam)))
+		{
+			qpar->param_type = QOF_TYPE_INT64;
+			j++;
+		} 
+		else
+		if (G_IS_PARAM_SPEC_BOOLEAN(gparam))
+		{
+			qpar->param_type = QOF_TYPE_BOOLEAN;
+			j++;
+		} 
+		else
+		if (G_IS_PARAM_SPEC_STRING(gparam))
+		{
+			qpar->param_type = QOF_TYPE_STRING;
+			j++;
+		} 
+		else
+		if ((G_IS_PARAM_SPEC_POINTER(gparam)) ||
+		    (G_IS_PARAM_SPEC_OBJECT(gparam)))
+		{
+			/* No-op, silently ignore.  Someday we should handle this ...  */
+		} 
+		else
+		if ((G_IS_PARAM_SPEC_FLOAT(gparam)) ||
+		    (G_IS_PARAM_SPEC_DOUBLE(gparam)))
+		{
+			qpar->param_type = QOF_TYPE_DOUBLE;
+			j++;
+		} 
+		else
+		if (G_IS_PARAM_SPEC_CHAR(gparam))
+		{
+			qpar->param_type = QOF_TYPE_CHAR;
+			j++;
+		} 
+		else
+		{
+			PWARN ("Unknown/unhandled parameter type %s on %s:%s\n", 
+			    G_PARAM_SPEC_TYPE_NAME(gparam), e_type, qpar->param_name);
 		}
 	}
-	/* NULL-terminaed list !! */
+
+	/* NULL-terminated list! */
 	qof_param_list[j].param_type = NULL;
 
    qof_class_register (e_type, NULL, qof_param_list);
@@ -232,26 +298,25 @@ printf ("its an int!! %s \n", qpar->param_name);
 
 /* ======================= END OF FILE ================================ */
 
+#include <qof/qof.h>
 #include <gtk/gtk.h>
 
 int
 main(int argc, char *argv[])
 {
 	g_type_init ();
-
 	gtk_init (&argc, &argv);
+	qof_gobject_init ();
 
+	/* Register a new searchable type */
 	GtkWidget *w = gtk_button_new_with_label ("Howdy Doody");
-
 	GtkButtonClass * wc = GTK_BUTTON_GET_CLASS(w);
-
 	GObjectClass *goc = G_OBJECT_CLASS(wc);
 
-	qof_gobject_init ();
 	qof_gobject_register ("MyGtkButton", goc);
 
+	/* Add a number of instances to the collection */
    QofBook *book =  qof_book_new();
-
 	qof_gobject_register_instance (book, "MyGtkButton", G_OBJECT(w));
 
 	w = gtk_button_new_with_label ("dorf");
@@ -260,21 +325,34 @@ main(int argc, char *argv[])
 	w = gtk_button_new_with_label ("zinger");
 	qof_gobject_register_instance (book, "MyGtkButton", G_OBJECT(w));
 
+	printf ("\nCreated three objects and registered them\n\n");
 
+   /* Create a new query, run that query */
 	QofSqlQuery *q;
-   /* Create a new query */
    q =  qof_sql_query_new ();
  
    /* Set the book to be searched */
    qof_sql_query_set_book(q, book);
 
-	GList *results = qof_sql_query_run (q, "SELECT * FROM GtkButton");
+	char * qstr = "SELECT * FROM MyGtkButton WHERE (label = 'dorf')";
+	printf ("Going to perform the query %s\n", qstr); 
 
-	printf ("got %d results \n", g_list_length (results));
+	GList *results = qof_sql_query_run (q, qstr);
+	printf ("Query returned %d results\n", g_list_length (results));
+
+	GList *n;
+	for (n=results; n; n=n->next)
+	{
+		GtkWidget *qw = n->data;
+		if (GTK_IS_BUTTON(qw))
+		{
+			GtkButton *bw = GTK_BUTTON (qw);
+			printf ("Found a button whose label is %s\n", gtk_button_get_label(bw));
+		}
+	}
 
 	qof_gobject_shutdown ();
 	return 0;
 }
-
 
 /* =================== END OF FILE ===================== */
