76ed3c7dd7a630bac00ccb2d8da1ddf50a2d0e4c
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-04-20 17:31:41 +0000

    fix typo

--- qof/examples/my-object.h
+++ qof/examples/my-object.h
@@ -4,7 +4,7 @@
  *  @author Copyright (c) 2003 Linas Vepstas <linas@linas.org>
  *
  *  This example program shows how to configure an arbitrary
- *  programmer-defined oject "MyObj" so that the Query routines
+ *  programmer-defined object "MyObj" so that the Query routines
  *  can be used on it.  It shows one part of the total:
  *  -- The object definition, showing how to hook into the query system.
  */
