519b5e9a39d2d19018834c809e35ab28742fe85a
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-05-01 05:03:28 +0000

    get to going

--- qof/src/qofquery-serialize.c
+++ qof/src/qofquery-serialize.c
@@ -1,6 +1,8 @@
 
 /* 
  * Take a query, serialize it into XML
+
+ * Copyright (C) 2001,2002,2004 Linas Vepstas <linas@linas.org>
  */
 #include <stdio.h>
 
@@ -9,11 +11,131 @@
 
 #include <libxml/tree.h>
 
+/* ======================================================= */
+
+#define PUT_STR(TOK,VAL) {                           \
+   xmlNodePtr node;                                  \
+   const char * str = (VAL);                         \
+   if (str && 0 != str[0])                           \
+   {                                                 \
+      node = xmlNewNode (NULL, TOK);                 \
+      xmlNodeAddContent(node, str);                  \
+      xmlAddChild (topnode, node);                   \
+   }                                                 \
+}
+
+#define PUT_INT(TOK,VAL) {                           \
+   xmlNodePtr node;                                  \
+   char buff[80];                                    \
+   g_snprintf (buff, sizeof(buff), "%d", (VAL));     \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, buff);                    \
+   xmlAddChild (topnode, node);                      \
+}
+
+#define PUT_LONG(TOK,VAL) {                          \
+   xmlNodePtr node;                                  \
+   char buff[80];                                    \
+   g_snprintf (buff, sizeof(buff), "%ld", (VAL));    \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, buff);                    \
+   xmlAddChild (topnode, node);                      \
+}
+
+
+#define PUT_DBL(TOK,VAL) {                           \
+   xmlNodePtr node;                                  \
+   char buff[80];                                    \
+   g_snprintf (buff, sizeof(buff), "%.18g", (VAL));  \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, buff);                    \
+   xmlAddChild (topnode, node);                      \
+}
+
+#define PUT_GUID(TOK,VAL) {                          \
+   xmlNodePtr node;                                  \
+   char buff[80];                                    \
+   guid_to_string_buff ((VAL), buff);                \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, buff);                    \
+   xmlAddChild (topnode, node);                      \
+}
+
+#define PUT_BOOL(TOK,VAL) {                          \
+   xmlNodePtr node;                                  \
+   gboolean boll = (VAL);                            \
+   node = xmlNewNode (NULL, TOK);                    \
+   if (boll) {                                       \
+      xmlNodeAddContent(node, "T");                  \
+   } else {                                          \
+      xmlNodeAddContent(node, "F");                  \
+   }                                                 \
+   xmlAddChild (topnode, node);                      \
+}
+
+#define PUT_ENUM_3(TOK,VAL,A,B,C) {                  \
+   xmlNodePtr node;                                  \
+   const char * str = #A;                            \
+   switch (VAL)                                      \
+   {                                                 \
+      case GTT_##A: str = #A; break;                 \
+      case GTT_##B: str = #B; break;                 \
+      case GTT_##C: str = #C; break;                 \
+   }                                                 \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, str);                     \
+   xmlAddChild (topnode, node);                      \
+}
+
+#define PUT_ENUM_4(TOK,VAL,A,B,C, D) {               \
+   xmlNodePtr node;                                  \
+   const char * str = #A;                            \
+   switch (VAL)                                      \
+   {                                                 \
+      case GTT_##A: str = #A; break;                 \
+      case GTT_##B: str = #B; break;                 \
+      case GTT_##C: str = #C; break;                 \
+      case GTT_##D: str = #D; break;                 \
+   }                                                 \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, str);                     \
+   xmlAddChild (topnode, node);                      \
+}
+
+#define PUT_ENUM_5(TOK,VAL,A,B,C,D,E) {              \
+   xmlNodePtr node;                                  \
+   const char * str = #A;                            \
+   switch (VAL)                                      \
+   {                                                 \
+      case GTT_##A: str = #A; break;                 \
+      case GTT_##B: str = #B; break;                 \
+      case GTT_##C: str = #C; break;                 \
+      case GTT_##D: str = #D; break;                 \
+      case GTT_##E: str = #E; break;                 \
+   }                                                 \
+   node = xmlNewNode (NULL, TOK);                    \
+   xmlNodeAddContent(node, str);                     \
+   xmlAddChild (topnode, node);                      \
+}
+
 static xmlNodePtr
-do_qof_query_to_xml (QofQuery *q)
+qof_query_terms_to_xml (QofQuery *q)
 {
-	// topnode = xmlNewNode (NULL, "gtt:project");
 	return NULL;
+}
+
+void
+do_qof_query_to_xml (QofQuery *q, xmlNodePtr topnode)
+{
+	QofIdType search_for = qof_query_get_search_for (q);
+	PUT_STR ("search-for", search_for);
+
+	xmlNodePtr *terms = qof_query_terms_to_xml(q);
+	if (terms) xmlAddChild (topnode, terms);
+
+	gint max_results = qof_query_get_max_results (q);
+	PUT_INT ("max-results", max_results);
+
 }
 
 xmlNodePtr
@@ -29,8 +151,7 @@ qof_query_to_xml (QofQuery *q)
 	// XXX path to DTD is wrong
 	// ns = xmlNewNs (topnode, "file:" "/usr/share/lib" "/qofquery.dtd", "qof");
 
-	node = do_qof_query_to_xml (q);
-	if (node) xmlAddChild (topnode, node);
+	do_qof_query_to_xml (q, topnode);
 
 	return topnode;
 }
