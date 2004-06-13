5c15b2ecd98ab94fb8fc8b3952745e3411ee2967
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-06-13 02:39:58 +0000

    move the files over from teh src directory

---
+++ qof/qof/qofnumeric.h
@@ -0,0 +1,232 @@
+/********************************************************************
+ * gnc-numeric.h - An exact-number library for gnucash              *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+ *******************************************************************/
+
+/** @addtogroup Numeric
+    The 'Numeric' functions provide a way of working with rational
+    numbers while maintaining strict control over rounding errors
+    when adding rationals with different denominators.  The Numeric
+    class is primarily used for working with monetary amounts, 
+    where the denominator typically represents the smallest fraction
+    of the currency (e.g. pennies, centimes).  The numeric class
+    can handle any fraction (e.g. twelfth's) and is not limited
+    to fractions that are powers of ten.
+
+    Please refer to the GnuCash texinfo documentation for details 
+    on the numeric functions.  (The texinfo will someday be
+    brought inline, here, into this file).
+
+    @{ */
+/** @file gnc-numeric.h
+    @brief An exact-rational-number library for gnucash.
+    @author Copyright (C) 2000 Bill Gribble
+*/
+
+#ifndef GNC_NUMERIC_H
+#define GNC_NUMERIC_H
+
+#include <glib.h>
+
+struct _gnc_numeric {
+  gint64  num;
+  gint64  denom;
+};
+
+/** @brief An exact-number type
+ *
+ * This is a rational number, defined by nominator and denominator. */
+typedef struct _gnc_numeric gnc_numeric;
+
+/** bitmasks for HOW flags.
+ * bits 8-15 of 'how' are reserved for the number of significant
+ * digits to use in the output with GNC_DENOM_SIGFIG */ 
+
+#define GNC_NUMERIC_RND_MASK     0x0000000f
+#define GNC_NUMERIC_DENOM_MASK   0x000000f0
+#define GNC_NUMERIC_SIGFIGS_MASK 0x0000ff00
+
+/** rounding/truncation modes for operations */
+enum { 
+  GNC_RND_FLOOR            = 0x01, 
+  GNC_RND_CEIL             = 0x02,  
+  GNC_RND_TRUNC            = 0x03,
+  GNC_RND_PROMOTE          = 0x04,
+  GNC_RND_ROUND_HALF_DOWN  = 0x05, 
+  GNC_RND_ROUND_HALF_UP    = 0x06, 
+  GNC_RND_ROUND            = 0x07, 
+  GNC_RND_NEVER            = 0x08
+};
+
+/** auto-denominator types */
+enum { 
+  GNC_DENOM_EXACT  = 0x10, 
+  GNC_DENOM_REDUCE = 0x20,
+  GNC_DENOM_LCD    = 0x30,
+  GNC_DENOM_FIXED  = 0x40,
+  GNC_DENOM_SIGFIG = 0x50
+};
+
+/** Error codes */
+typedef enum {
+  GNC_ERROR_OK         =  0,   /**< No error */
+  GNC_ERROR_ARG        = -1,   /**< Argument is not a valid number */
+  GNC_ERROR_OVERFLOW   = -2,   /**< Intermediate result overflow */
+  GNC_ERROR_DENOM_DIFF = -3,   /**< Argument denoms differ in GNC_DENOM_FIXED operation */
+  GNC_ERROR_REMAINDER  = -4    /**< Remainder part in GNC_RND_NEVER operation */
+} GNCNumericErrorCode;
+
+#define GNC_DENOM_AUTO 0
+
+#define GNC_DENOM_RECIPROCAL( a ) (- ( a ))
+#define GNC_DENOM_SIGFIGS( a ) ( ((( a ) & 0xff) << 8) | GNC_DENOM_SIGFIG)
+#define GNC_NUMERIC_GET_SIGFIGS( a ) ( (( a ) & 0xff00 ) >> 8)
+
+/** @name Constructors */
+/*@{*/
+/** make a gnc_numeric from numerator and denominator */
+gnc_numeric gnc_numeric_create(gint64 num, gint64 denom);
+
+/** create a zero-value gnc_numeric */
+gnc_numeric gnc_numeric_zero(void);
+
+/** convert from floating-point values */
+gnc_numeric double_to_gnc_numeric(double in, gint64 denom,  
+                                  gint how);
+
+/** Read a gnc_numeric from str, skipping any leading whitespace, and
+   returning a pointer to just past the last byte read.  Return NULL
+   on error. */
+const gchar *string_to_gnc_numeric(const gchar* str, gnc_numeric *n);
+
+/** make a special error-signalling gnc_numeric */
+gnc_numeric gnc_numeric_error(GNCNumericErrorCode error_code);
+/*@}*/
+
+/** @name Value accessors */
+/*@{*/
+/** Get parts */
+gint64 gnc_numeric_num(gnc_numeric a);
+/** Get parts */
+gint64 gnc_numeric_denom(gnc_numeric a);
+
+/** Convert to floating-point values */
+double      gnc_numeric_to_double(gnc_numeric in);
+
+/** Convert to string. The returned buffer is to be g_free'd by the
+ * caller (it was allocated through g_strdup) */
+gchar *gnc_numeric_to_string(gnc_numeric n);
+/*@}*/
+
+/** @name Tests */
+/*@{*/
+/** Check for error signal in value. Returns GNC_ERROR_OK (==0) if
+ *  the number appears to be valid, otherwise it returns the
+ *  type of error.
+ */ 
+GNCNumericErrorCode  gnc_numeric_check(gnc_numeric a);
+
+/** Returns 1 if the given gnc_numeric is 0 (zeros), else returns 0. */
+int gnc_numeric_zero_p(gnc_numeric a);                
+/** Returns 1 if a>b, -1 if b>a, 0 if a == b  */
+int gnc_numeric_compare(gnc_numeric a, gnc_numeric b);
+int gnc_numeric_negative_p(gnc_numeric a);
+int gnc_numeric_positive_p(gnc_numeric a);
+
+/** Equivalence predicate: Returns TRUE (1) if a and b are exactly the
+ * same (same numerator and denominator)
+ */ 
+int gnc_numeric_eq(gnc_numeric a, gnc_numeric b);     
+
+/** Equivalence predicate: Returns TRUE (1) if a and b represent
+ *  exactly the same number (ratio of numerator to denominator is
+ *  exactly equal)
+ */ 
+int gnc_numeric_equal(gnc_numeric a, gnc_numeric b);  
+
+/** Equivalence predicate: Returns TRUE (1) if after both are
+ * converted to DENOM using method HOW, a and b are
+ * gnc_numeric_equal().
+ */ 
+int gnc_numeric_same(gnc_numeric a, gnc_numeric b,   
+                     gint64 denom, gint how);
+/*@}*/
+
+/** @name Arithmetic operations */
+/*@{*/
+gnc_numeric gnc_numeric_add(gnc_numeric a, gnc_numeric b, 
+                            gint64 denom, gint how);
+gnc_numeric gnc_numeric_sub(gnc_numeric a, gnc_numeric b, 
+                            gint64 denom, gint how);
+gnc_numeric gnc_numeric_mul(gnc_numeric a, gnc_numeric b, 
+                            gint64 denom, gint how);
+gnc_numeric gnc_numeric_div(gnc_numeric a, gnc_numeric b, 
+                            gint64 denom, gint how);
+/** Negate the argument  */
+gnc_numeric gnc_numeric_neg(gnc_numeric a);
+/** Return the absolute value of the argument */
+gnc_numeric gnc_numeric_abs(gnc_numeric a);
+
+/** 
+ * Shortcut for most common case: gnc_numeric_add(a, b, GNC_DENOM_AUTO,
+ *                        GNC_DENOM_FIXED | GNC_RND_NEVER);
+ */
+gnc_numeric gnc_numeric_add_fixed(gnc_numeric a, gnc_numeric b);
+/** 
+ * Shortcut for most common case: gnc_numeric_sub(a, b, GNC_DENOM_AUTO,
+ *                        GNC_DENOM_FIXED | GNC_RND_NEVER);
+ */
+gnc_numeric gnc_numeric_sub_fixed(gnc_numeric a, gnc_numeric b);
+/*@}*/
+
+/** @name Arithmetic functions with exact error returns */
+/*@{*/
+gnc_numeric gnc_numeric_add_with_error(gnc_numeric a, gnc_numeric b,
+                                       gint64 denom, gint how,
+                                       gnc_numeric * error);
+gnc_numeric gnc_numeric_sub_with_error(gnc_numeric a, gnc_numeric b,
+                                       gint64 denom, gint how,
+                                       gnc_numeric * error);
+gnc_numeric gnc_numeric_mul_with_error(gnc_numeric a, gnc_numeric b,
+                                       gint64 denom, gint how,
+                                       gnc_numeric * error);
+gnc_numeric gnc_numeric_div_with_error(gnc_numeric a, gnc_numeric b,
+                                       gint64 denom, gint how,
+                                       gnc_numeric * error);
+/*@}*/
+
+/** @name Change denominator */
+/*@{*/
+/** change the denominator of a gnc_numeric value */
+gnc_numeric gnc_numeric_convert(gnc_numeric in, gint64 denom, 
+                                gint how);
+
+/** change the denominator of a gnc_numeric value */
+gnc_numeric gnc_numeric_convert_with_error(gnc_numeric in, gint64 denom, 
+                                           gint how,
+                                           gnc_numeric * error);
+
+/** reduce by GCF elimination */
+gnc_numeric gnc_numeric_reduce(gnc_numeric in);
+/*@}*/
+
+
+#endif
+
+/*@}*/
---
+++ qof/qof/qofutil.h
@@ -0,0 +1,139 @@
+/********************************************************************\
+ * gnc-engine-util.h -- GnuCash engine utility functions            *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+\********************************************************************/
+
+/** @addtogroup Utilities
+    @{ */
+/** @file gnc-engine-util.h 
+    @brief GnuCash engine utility functions 
+    @author Copyright (C) 1997 Robin D. Clark <rclark@cs.hmc.edu>
+    @author Copyright (C) 2000 Bill Gribble <grib@billgribble.com>
+    @author Copyright (C) 1997-2002,2004 Linas Vepstas <linas@linas.org>
+*/
+
+#ifndef QOF_UTIL_H
+#define QOF_UTIL_H
+
+#include <glib.h>
+#include <stddef.h>
+
+#include "gnc-trace.h"  // XXX eliminate me eventually
+
+/** Macros *****************************************************/
+
+#define SAFE_STRCMP_REAL(fcn,da,db) {    \
+  if ((da) && (db)) {                    \
+    if ((da) != (db)) {                  \
+      int retval = fcn ((da), (db));     \
+      /* if strings differ, return */    \
+      if (retval) return retval;         \
+    }                                    \
+  } else                                 \
+  if ((!(da)) && (db)) {                 \
+    return -1;                           \
+  } else                                 \
+  if ((da) && (!(db))) {                 \
+    return +1;                           \
+  }                                      \
+}
+
+#define SAFE_STRCMP(da,db) SAFE_STRCMP_REAL(strcmp,(da),(db))
+#define SAFE_STRCASECMP(da,db) SAFE_STRCMP_REAL(strcasecmp,(da),(db))
+
+/* Define the long long int conversion for scanf */
+#if HAVE_SCANF_LLD
+# define GNC_SCANF_LLD "%lld"
+#else
+# define GNC_SCANF_LLD "%qd"
+#endif
+
+
+/** Prototypes *************************************************/
+
+/** The safe_strcmp compares strings a and b the same way that strcmp()
+ * does, except that either may be null.  This routine assumes that
+ * a non-null string is always greater than a null string.
+ */
+int safe_strcmp (const char * da, const char * db);
+int safe_strcasecmp (const char * da, const char * db);
+
+/** The null_strcmp compares strings a and b the same way that strcmp()
+ * does, except that either may be null.  This routine assumes that
+ * a null string is equal to the empty string.
+ */
+int null_strcmp (const char * da, const char * db);
+
+/** Search for str2 in first nchar chars of str1, ignore case. Return
+ * pointer to first match, or null. These are just like that strnstr
+ * and the strstr functions, except that they ignore the case. */
+extern char *strncasestr(const char *str1, const char *str2, size_t len);
+extern char *strcasestr(const char *str1, const char *str2);
+
+/** The ultostr() subroutine is the inverse of strtoul(). It accepts a
+ * number and prints it in the indicated base.  The returned string
+ * should be g_freed when done.  */
+char * ultostr (unsigned long val, int base);
+
+/** Returns true if string s is a number, possibly surrounded by
+ * whitespace. */
+gboolean gnc_strisnum(const char *s);
+
+/** Local copy of stpcpy, used wtih libc's that don't have one. */
+char * gnc_stpcpy (char *dest, const char *src);
+
+#ifndef HAVE_STPCPY
+#define stpcpy gnc_stpcpy
+#endif
+
+/** Return NULL if the field is whitespace (blank, tab, formfeed etc.)  
+ *  Else return pointer to first non-whitespace character. 
+ */
+const char * qof_util_whitespace_filter (const char * val);
+
+/** Return integer 1 if the string starts with 't' or 'T' or 
+ *  contains the word 'true' or 'TRUE'; if string is a number, 
+ *  return that number. (Leading whitespace is ignored). */
+int qof_util_bool_to_int (const char * val);
+
+/** Many strings used throughout the engine are likely to be duplicated.
+ * So we provide a reference counted cache system for the strings, which
+ * shares strings whenever possible.
+ *
+ * Use g_cache_insert to insert a string into the cache (it will return a
+ * pointer to the cached string).
+ * Basically you should use this instead of g_strdup.
+ *
+ * Use g_cache_remove (giving it a pointer to a cached string) if the string
+ * is unused.  If this is the last reference to the string it will be
+ * removed from the cache, otherwise it will just decrement the
+ * reference count.
+ * Basically you should use this instead of g_free.
+ *
+ * Note that all the work is done when inserting or removing.  Once
+ * cached the strings are just plain C strings.
+ */
+
+/** Get the gnc_string_cache.  Create it if it doesn't exist already */
+GCache* gnc_engine_get_string_cache(void);
+
+void gnc_engine_string_cache_destroy (void);
+
+#endif /* QOF_UTIL_H */
+/** @} */
