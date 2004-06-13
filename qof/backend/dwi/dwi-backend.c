3de2a63ea6d2464ef7234c5f8940a44b9a74cbba
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-06-13 20:34:56 +0000

    start adding example code showing how to use the backend

--- qof/backend/dwi/dwi-backend.c
+++ qof/backend/dwi/dwi-backend.c
@@ -14,24 +14,47 @@ typedef struct DwiBackend_s DwiBackend;
 
 /* ============================================================= */
 
-void dwiend_init (DwiBackend *be)
+static void 
+dwiend_init (DwiBackend *be)
 {
 	qof_backend_init((QofBackend*)be);
 }
 
 /* ============================================================= */
 
-QofBackend *
+static QofBackend *
 dwiend_new (void)
 {
 	DwiBackend *be;
 
 	ENTER(" ");
 	be = g_new0 (DwiBackend, 1);
-	dwiend_init (be);
 
 	LEAVE(" ")
 	return (QofBackend *) be;
+}
+
+/* ============================================================= */
+
+static void 
+dwiend_provider_free (QofBackendProvider *prov)
+{
+	prov->provider_name = NULL;
+	prov->access_method = NULL;
+	g_free (prov);
+}
+
+/* ============================================================= */
+
+void 
+dwiend_provider_init (void)
+{
+	QofBackendProvider *prov;
+	prov = g_new0 (QofBackendProvider, 1);
+	prov->provider_name = "DWI Backend Version 0.1";
+	prov->access_method = "sql";
+	prov->backend_new = dwiend_new;
+	prov->provider_free = dwiend_provider_free;
 }
 
 /* ======================== END OF FILE ========================= */
