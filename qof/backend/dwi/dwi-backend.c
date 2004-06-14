1061a688d68aecb11206066982b7719556a80c48
linas <linas@62108737-c419-47b0-bd6d-f326680ffe65>
2004-06-14 01:34:34 +0000

    start getting a backend that actually loads and runns

--- qof/backend/dwi/dwi-backend.c
+++ qof/backend/dwi/dwi-backend.c
@@ -1,4 +1,5 @@
 
+#include <dwi/database.h>
 #include <qof/qofbackend.h>
 #include <qof/qofbackend-p.h>
 #include <qof/gnc-trace.h>
@@ -8,16 +9,40 @@ static short module = MOD_BACKEND;
 struct DwiBackend_s 
 {
 	QofBackend be;
+	DuiDatabase *db;
 };
 
 typedef struct DwiBackend_s DwiBackend;
 
 /* ============================================================= */
 
+static void
+dwiend_session_begin (QofBackend *qbe,
+                      QofSession *session,
+                      const char *book_id,
+                      gboolean ignore_lock,
+                      gboolean create_if_nonexistent)
+{
+	DwiBackend *be = (DwiBackend *) qbe;
+
+	printf ("duude ses start wi/ id=%s\n", book_id);
+
+	be->db = dui_database_new ("my db object",
+	                          // "libdbi", "my-qof-db",
+	                          // "odbc", "my-qof-db",
+	                          "libpg", "my-qof-db",
+	                          // "127.0.0.1", "linas", "abc123");
+	                          NULL, "linas", NULL);
+}
+
+/* ============================================================= */
+
 static void 
 dwiend_init (DwiBackend *be)
 {
-	qof_backend_init((QofBackend*)be);
+	qof_backend_init (&be->be);
+	be->db = NULL;
+	be->be.session_begin = dwiend_session_begin;
 }
 
 /* ============================================================= */
@@ -29,6 +54,7 @@ dwiend_new (void)
 
 	ENTER(" ");
 	be = g_new0 (DwiBackend, 1);
+	dwiend_init (be);
 
 	LEAVE(" ")
 	return (QofBackend *) be;
@@ -55,6 +81,7 @@ dwiend_provider_init (void)
 	prov->access_method = "sql";
 	prov->backend_new = dwiend_new;
 	prov->provider_free = dwiend_provider_free;
+	qof_backend_register_provider (prov);
 }
 
 /* ======================== END OF FILE ========================= */
