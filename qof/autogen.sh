ad16fbf80d902ecddff87f1ddb373561277e0799
codehelpgpg <codehelpgpg@62108737-c419-47b0-bd6d-f326680ffe65>
2005-01-30 22:37:17 +0000

    automated config script for external package detection

--- qof/autogen.sh
+++ qof/autogen.sh
@@ -102,7 +102,7 @@ do
     echo processing $dr
     ( cd $dr
 
-      aclocalinclude="$ACLOCAL_FLAGS"
+      aclocalinclude="-I m4 $ACLOCAL_FLAGS"
 
       if grep "^AM_GLIB_GNU_GETTEXT" configure.in >/dev/null; then
 	echo "Creating $dr/aclocal.m4 ..."
