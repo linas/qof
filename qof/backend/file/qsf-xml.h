f590b4bc2aeeecbf2f2b2b5357c18b21a566be8a
codehelpgpg <codehelpgpg@62108737-c419-47b0-bd6d-f326680ffe65>
2006-07-09 12:51:08 +0000

    Doxygen tweak.

--- qof/backend/file/qsf-xml.h
+++ qof/backend/file/qsf-xml.h
@@ -652,8 +652,8 @@ otherwise FALSE.
 gboolean is_qsf_object_with_map_be (gchar * map_path, qsf_param * params);
 /** \brief Validate a QSF file and a selected QSF map
 
-@param	map_path	Absolute or relative path to the selected QSF map file
-@param	params	Pointer to qsf_param context
+@param	path	Absolute or relative path to the selected QSF object file
+@param	map_file Name of the QSF map file, located in QSF_SCHEMA_DIR.
 
 The file is validated against the QSF object schema, qsf-object.xsd.xml and
 each object described in the file is checked to find out if the supplied QSF
