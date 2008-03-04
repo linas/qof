363322455c856c63b5699ad38bdc7dacf9efc28b
codehelpgpg <codehelpgpg@62108737-c419-47b0-bd6d-f326680ffe65>
2008-03-04 09:41:42 +0000

    use autoreconf instead of autogen.sh

--- qof/autogen.sh
+++ qof/autogen.sh
@@ -1,29 +1,6 @@
 #!/bin/sh -x
-
-# Exit this script if any command fails with non-zero exit status.
 set -e
 
-# First cache the command names in variables. If you want to
-# override the names, simply set the variables before calling this
-# script.
-
-: ${GETTEXTIZE=gettextize}
-: ${LIBTOOLIZE=libtoolize}
-: ${ACLOCAL=aclocal}
-: ${AUTOHEADER=autoheader}
-: ${AUTOMAKE=automake}
-: ${AUTOCONF=autoconf}
-
-if [ ! -e ABOUT-NLS ]; then
-	${GETTEXTIZE} -f
-fi
-rm -f intltool*
-rm -f po/Makevars.template
-${LIBTOOLIZE} --force --automake
-${ACLOCAL} ${ACLOCAL_FLAGS}
-${AUTOHEADER}
-${AUTOMAKE} --add-missing
-${AUTOCONF}
+autoreconf -ifs
 
 echo "Now you can run ./configure --enable-error-on-warning --enable-compile-warnings"
-
