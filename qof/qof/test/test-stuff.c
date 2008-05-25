3a40e8798259627cb4353d038ba462f3b53323fd
codehelp <codehelp@62108737-c419-47b0-bd6d-f326680ffe65>
2008-05-25 12:17:24 +0000

    tweak

--- qof/qof/test/test-stuff.c
+++ qof/qof/test/test-stuff.c
@@ -1,4 +1,19 @@
 /*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+/*
  * Created 20010320 by bstanley to hold only those
  * testing functions which are independent of the rest of
  * the GNUCash system.
@@ -247,7 +262,7 @@ get_random_string_without (const char *exclude_chars)
 
 	for (i = 0; i < len - 1; i++)
 	{
-		char c;
+		gchar c;
 
 		do
 		{
