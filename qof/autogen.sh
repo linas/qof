576272753f5caf5f47569f4a0572f42cc3675be6
codehelp <codehelp@62108737-c419-47b0-bd6d-f326680ffe65>
2011-02-20 17:07:33 +0000

    finalise for 0.8.2 release

--- qof/autogen.sh
+++ qof/autogen.sh
@@ -1,7 +1,10 @@
-#!/bin/sh -x
+#!/bin/sh
 set -e
 
-echo autoreconf requires autopoint
+if [ ! -x /usr/bin/autopoint ]; then
+	echo autoreconf requires autopoint because QOF uses gettext
+	exit 1
+fi
 autoreconf -ifs
-
 echo "Now you can run ./configure --enable-error-on-warning --enable-compile-warnings"
+
